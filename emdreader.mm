0990480d580edb1d4249b003773f696df082f575
PoomSmart <thatchapon.unp@outlook.com>
2019-10-23 15:46:32 +0700

    Now know more bits

--- emdreader.mm
+++ emdreader.mm
@@ -1,6 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+bool hasSkin(uint32_t v) {
+    return v & 0x40;
+}
+
+int skinTone(uint32_t v) {
+    return hasSkin(v) ? 0 : MAX(2, v >> 20) - 1;
+}
+
+bool hasHair(uint32_t v) {
+    return v & 0x100;
+}
+
+int hairStyle(uint32_t v) {
+    return v & 0xF;
+}
+
+// TODO: correct?
+int presentationStyle(uint32_t v) {
+    if (v & 0x20)
+        return 1;
+    if (v & 0x10)
+        return 2;
+    return 0;
+}
+
+char gender(uint32_t v) {
+    if (v & 0x20000)
+        return 'F';
+    if (v & 0x10000)
+        return 'M';
+    return '-';
+}
+
+void readStr(FILE *fp, char *emoji) {
+    char b;
+    size_t n = 0;
+    while ((b = fgetc(fp)) != EOF && b != '\0')
+        emoji[n++] = (unsigned char)b;
+    emoji[n] = '\0';
+}
+
 // TODO: Support iOS < 12.1 emoji metadata ?
 int main(int argc, char *argv[], char *envp[]) {
     if (argc != 2) {
@@ -24,24 +65,35 @@ int main(int argc, char *argv[], char *envp[]) {
     printf("Emoji string pointer: %x\n", reptr);
     fread(buf, 2, 1, fp);
     printf("Taiwan flag index: %u\n", buf[0]); // TODO: is it?
-    fseek(fp, reptr + 8, SEEK_SET);
 
-    // [ F09F 9880 ] 00 [ F0 9F98 AC ] 00 F09F 9881 00F0 9F98 8200 F09F 9883 ...
+    // unknown 3 bytes skipped
+    uint16_t metaptr = 7;
+    uint32_t metadata[4];
 
-    char hexstr[64];
+    char emoji[64];
+    char desc[256];
     int16_t index = 1;
-    char b;
     while (index <= count) {
-        size_t n = 0;
-        while ((b = fgetc(fp)) != EOF && b != '\0')
-            hexstr[n++] = (unsigned char)b;
-        hexstr[n] = '\0';
-        CFStringRef str = CFStringCreateWithCString(kCFAllocatorDefault, hexstr, kCFStringEncodingUTF8);
-        if (str) {
-            NSLog(@"%@", str);
-            CFRelease(str);
+        fseek(fp, metaptr, SEEK_SET);
+        fread(metadata, sizeof(uint32_t), 4, fp);
+        uint32_t emojiptr = metadata[2] >> 8;
+        fseek(fp, emojiptr, SEEK_SET);
+        readStr(fp, emoji);
+        CFStringRef cemoji = CFStringCreateWithCString(kCFAllocatorDefault, emoji, kCFStringEncodingUTF8);
+        if (cemoji) {
+            uint32_t d0 = metadata[0] >> 8;
+            uint32_t d1 = metadata[1];
+            uint32_t descPos = metadata[3] >> 8;
+            fseek(fp, descPos, SEEK_SET);
+            readStr(fp, desc);
+            if (!strlen(desc))
+                strcpy(desc, "<none>");
+            // emoji : variant meta(+) str-pos desc-pos (...)
+            NSLog(@"%@  :  0x%-10x  0x%-8x  [0x%x]  [0x%x]  (skin: %d-%d, hair: %d-%d, gender: %c, style: %d, desc: %s)\n", cemoji, d0, d1, emojiptr, descPos, hasSkin(d0), skinTone(d0), hasHair(d0), hairStyle(d1), gender(d0), presentationStyle(d0), desc);
+            CFRelease(cemoji);
         }
         ++index;
+        metaptr += 16;
     }
 
     fclose(fp);
