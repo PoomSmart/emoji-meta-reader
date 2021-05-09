afe0b455e0e20cf1dc9be02222d74a6514fce274
PoomSmart <thatchapon.unp@outlook.com>
2021-05-09 21:06:59 +0700

    Added generator

---
+++ generator.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z $1 ];then
+  echo "emojimeta.dat path required"
+  exit 1
+fi
+
+./bin/emdreader -r 2 -i $1 -e 1 -o ./emojimeta_legacy.dat
+./bin/emdreader -r 2 -i $1 -e 0 -o ./emojimeta_legacy_10.dat
+cp -fv ./emojimeta_legacy.dat ../EmojiPort-10-Resources/layout/System/Library/PrivateFrameworks/CoreEmoji.framework/emojimeta_legacy.dat
+cp -fv ./emojimeta_legacy_10.dat ../EmojiPort-10-Resources/layout/System/Library/PrivateFrameworks/CoreEmoji.framework/emojimeta_legacy_10.dat
+cp -fv $1 ../EmojiPort-10-Resources/layout/System/Library/PrivateFrameworks/CoreEmoji.framework/emojimeta_modern.dat
